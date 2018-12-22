c512c2bac31e938d9ba49b272fcb26e8642d40a2
Marco Bruno <marco.bruno.br@gmail.com>
2018-12-22 14:47:48 +0000

    Decima quinta aula

--- js/description-product.js
+++ js/description-product.js
@@ -1,7 +1,48 @@
-const $heart = window.document.querySelector(".-heart");
+const $heart = document.querySelector(".-heart");
+const $stars = document.querySelectorAll(".star");
+const positionLast = $stars.length - 1;
 
 $heart.addEventListener("click", handleClick);
 
+$stars.forEach(function($star, key) {
+  if (key == 0) {
+    $star.addEventListener("click", firstStar);
+  }
+
+  if (key == positionLast) {
+    $star.addEventListener("click", lastStar);
+  }
+
+  if (key > 0 && key < positionLast) {
+    $star.addEventListener("click", function() {
+      middleStar(key);
+    });
+  }
+});
+
 function handleClick() {
-  $heart.classList.toggle("-active");
+  this.classList.toggle("-active");
+}
+
+function firstStar() {
+  $stars.forEach(function($star) {
+    $star.classList.remove("-active");
+  });
+  this.classList.add("-active");
+}
+
+function lastStar() {
+  $stars.forEach(function($star) {
+    $star.classList.add("-active");
+  });
+}
+
+function middleStar(index) {
+  $stars.forEach(function($star, key) {
+    $star.classList.remove("-active");
+
+    if (key <= index) {
+      $star.classList.add("-active");
+    }
+  });
 }
